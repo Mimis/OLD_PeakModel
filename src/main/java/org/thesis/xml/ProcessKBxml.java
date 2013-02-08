123cbb253561795ff9cb5ca7919612245a105440
Mimis <dimitriskarapapas@gmail.com>
2013-02-08 16:44:45 +0100

    fix bug on  removeAttrNameSpaces

--- src/main/java/org/thesis/xml/ProcessKBxml.java
+++ src/main/java/org/thesis/xml/ProcessKBxml.java
@@ -5,23 +5,20 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ProcessKBxml {
 	public static void main(String args[]) throws IOException {
-		String fileToRead = "./data/1991.xml";
 		removeXmlAttributeNamespaces(args[0]);
-//		TokenizeString(string, false, false);
 	}
 	
     
-	public static void removeXmlAttributeNamespaces(String xmlText) throws IOException{
+	public static void removeXmlAttributeNamespaces(String fileForProcessing) throws IOException{
 		long startTime = System.currentTimeMillis();
 
-		String fileToReplace = xmlText;
+		String fileToReplace = fileForProcessing;
 		File file = new File(fileToReplace);
-		File tempFile = new File("temp.txt");
+		File tempFile = new File(file.getParent()+"/temp.txt");
+		
 
 		BufferedReader reader = new BufferedReader(
 				new FileReader(fileToReplace));
@@ -50,47 +47,4 @@ public class ProcessKBxml {
 	    System.out.println("#Total run time:"+ (endTime-startTime)/1000);
 
 	}
-	
-	public static List<String> TokenizeString(String string,boolean omitDigits,boolean omitPunct) {
-		List<String> tokens = new ArrayList<String>();
-		
-		if(string == null || string.matches("\\s*") )
-			return null;
-		
-		int cursor = 0;
-		while (cursor < string.length()) {
-			char ch = string.charAt(cursor);
-			
-			if (Character.isWhitespace(ch)) {
-				cursor++;
-			} 
-			else if (Character.isLetter(ch)) {
-				StringBuilder buf = new StringBuilder();
-				while (cursor < string.length()	&& (Character.isLetter(string.charAt(cursor)) || Character.isDigit(string.charAt(cursor)))) {
-					buf.append(string.charAt(cursor));
-					cursor++;
-				}
-				String token = buf.toString();
-				tokens.add(token);
-			} 
-			else if (Character.isDigit(ch) && !omitDigits) {
-				StringBuilder buf = new StringBuilder();
-				while (cursor < string.length()	&& (Character.isLetter(string.charAt(cursor)) || Character.isDigit(string.charAt(cursor)))) {
-					buf.append(string.charAt(cursor));
-					cursor++;
-				}
-				String token = buf.toString();
-				tokens.add(token);
-			} 
-			else if(!omitPunct){
-				//SAVE PUNCT token
-				tokens.add(Character.toString(ch));
-				cursor++;
-			}
-			else
-				cursor++;
-		}
-		return tokens;
-	}
-
 }
